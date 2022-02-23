7f5fc57493d562bd075ea6ee2555696c682c0f74
lmacrown <lmacrown@naver.com>
2022-02-23 16:50:24 +0900

    15일차

--- typeconverter/src/main/java/hello/typeconverter/converter/StringToIpPortConverter.java
+++ typeconverter/src/main/java/hello/typeconverter/converter/StringToIpPortConverter.java
@@ -1,12 +1,9 @@
 package hello.typeconverter.converter;
-
 import hello.typeconverter.type.IpPort;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.convert.converter.Converter;
-
 @Slf4j
 public class StringToIpPortConverter implements Converter<String, IpPort> {
-
     @Override
     public IpPort convert(String source) {
         log.info("convert source={}", source);
